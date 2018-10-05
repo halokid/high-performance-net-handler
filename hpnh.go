ba76a912b33f559088c52857458923baf9d3d528
r00tjimmy <r00txxyyzz@gmail.com>
2018-10-05 16:06:38 +0800

    fix const varible

---
+++ hpnh.go
@@ -0,0 +1,10 @@
+package main 
+
+import (
+  "github.com/r00tjimmy/high-performance-net-handler/utils"
+  "fmt"
+)
+
+func main() {
+  fmt.Println(utils.Version)
+}
--- utils/common.go
+++ utils/common.go
@@ -15,12 +15,12 @@ func CheckErr(err error) {
 output debug info & save log file
  */
 func DebugLog(content string) {
-  if debug_flag == true {
+  if Debug_flag == true {
     log.Println(content)
   }
 
-  if log_flag == true {
-    log_file_handle, err := os.OpenFile(log_file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
+  if Log_flag == true {
+    log_file_handle, err := os.OpenFile(Log_file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
     CheckErr(err)
     log_file_handle.WriteString(content + "\n\n")
   }
