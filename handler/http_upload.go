81bcc9a94d96b52869388bc91e25d6c9bb33ecad
r00txx <82049406@qq.com>
2018-10-10 15:39:16 +0800

    done date-time folder upload files

--- handler/http_upload.go
+++ handler/http_upload.go
@@ -35,15 +35,44 @@ upload file to data-time folder
 func HttpUploadDateTimeHandle(w http.ResponseWriter, r *http.Request) {
   // check path
   exists, folder_path := DateTimeFolderExists()
-  if exists {
-    // folder exists
-
-  } else {
+  if !exists {
     // folder not exists, create
-    err := os.Mkdir(folder_path, os.ModePerm)
+    err := CreateDataFolder(folder_path)
     utils.CheckErr(err)
   }
 
+  // upload file
+  form_file, header, err := r.FormFile("uploadfile")
+  utils.CheckErr(err)
+  defer form_file.Close()
+
+  //create save file
+  dest_file, err := os.Create(utils.Http_upload_path + "/" + folder_path + "/" + header.Filename)
+  utils.CheckErr(err)
+  defer dest_file.Close()
+
+  // save file
+  _, err = io.Copy(dest_file, form_file)
+  utils.CheckErr(err)
+
+  fmt.Fprintf(w, "upload success")
+
+}
+
+
+
+func CreateDataFolder(folder_path string) error  {
+  date_folder := strings.Split(folder_path, "/")[0]
+  time_folder := strings.Split(folder_path, "/")[1]
+
+  if !utils.PathExists(utils.Http_upload_path + "/" + date_folder) {
+    err := os.Mkdir(utils.Http_upload_path + "/" + date_folder, os.ModePerm)
+    utils.CheckErr(err)
+  }
+
+  err := os.Mkdir(utils.Http_upload_path + "/" + date_folder + "/" + time_folder, os.ModePerm)
+  utils.CheckErr(err)
+  return  err
 }
 
 
@@ -73,12 +102,12 @@ func DateTimeFolderExists() (bool, string) {
   //fmt.Println(time_folder)
 
   folder_path := date_folder + "/" + time_folder
-  if utils.PathExists(date_folder + "/" + time_folder) {
-    //fmt.Println("folder exists")
-    return false, folder_path
-  } else {
-    //fmt.Println("folder not exists")
+  if utils.PathExists( utils.Http_upload_path + "/" + date_folder + "/" + time_folder) {
+    fmt.Println("folder exists")
     return true, folder_path
+  } else {
+    fmt.Println("folder not exists")
+    return false, folder_path
   }
 }
 
